004431d88e69f4585f31e21a7efcbea26d412d0e
Jkhall81 <jason.kei.hall@gmail.com>
2025-06-01 20:45:41 -0700

    last try for real this time.

--- config/app.php
+++ config/app.php
@@ -52,7 +52,7 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'http://localhost'),
+    'url' => env('APP_URL', 'https://safe-and-sound-crm.onrender.com'),
 
     /*
     |--------------------------------------------------------------------------
